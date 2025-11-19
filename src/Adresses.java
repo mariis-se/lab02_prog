3e4dff18294dec8edd11947dfaf8ddbc8f8829c7
Mariia <mvs6568708@gmail.com>
2025-11-19 23:05:36 +0300

    adresses class  commit

--- src/Adresses.java
+++ src/Adresses.java
@@ -1,3 +1,24 @@
 public class Adresses {
+    private String city;
+    private String street;
+    private String house;
+    private int floor;
 
+    public Adresses(String city, String street, String house, int floor){
+        this.city = city;
+        this.street = street;
+        this.house = house;
+        this.floor = floor;
+
+    }
+
+    public String getCity(){ return city; }
+    public String getStreet() {return street;}
+    public String getHouse() {return house;}
+    public int getFloor() {return floor;}
+
+    @Override
+    public String toString(){
+        return city + ", " + street + ", " + house + ", " + floor + " этажейй"
+    }
 }
