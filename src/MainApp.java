1c4c954d15c7acbf43e2b949fd6da46b3ddac21f
Mariia <mvs6568708@gmail.com>
2025-11-25 22:22:38 +0300

    mainapp Dublicates Statistics commit

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,21 +4,25 @@ import java.util.Scanner;
 public class MainApp{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("enter text parhfile: ");
+//        System.out.println("enter text parhfile: ");
+        boolean firstTry = true;
 
         while (true) {
+//            if (firstTry) {System.out.println("enter text parhfile: ");}
+            System.out.print("\nВведите путь к файлу или exit для выхода: ");
             String input = scanner.nextLine().trim();
 
             if (input.equalsIgnoreCase("exit")){
-                System.out.println("exit");
+                System.out.println("Завершение программы");
                 break;
             }
             if (input.isEmpty()){
                 continue;
             }
-            System.out.println("your enter: " + input);
+            System.out.println("Вы ввели: " + input);
 
             run(input);
+//            firstTry = false;
         }
         scanner.close();
     }
@@ -30,12 +34,14 @@ public class MainApp{
             Csv parser = new Csv();
             List<Addresses> addresses = parser.parseFile(filePath);
 
-            System.out.println("Done, amount: " + addresses.size());
+            System.out.println("Конец, размер: " + addresses.size());
 
-            System.out.println("\nfirst 5 lines:");
+            System.out.println("\nПервые пять строк (проверка): ");
             for (int i = 0; i < Math.min(5, addresses.size()); i++) {
                 System.out.println(addresses.get(i));
             }
+            System.out.println("\n");
+            DublicatesStatistics.findDubles(addresses);
 
 
         } catch (java.io.FileNotFoundException e) {
